3f97f2408d3254f806181045a637d0843e29e9f9
maizzon14 <mamami01@alumnatflorida.es>
2026-06-01 00:48:59 +0200

    Revert "Merge branch 'pruebas-union' into avance-mapa-aroa"
    This reverts commit 055b5f47b107b3b5780ff2e1c4570f54a28f1fcf, reversing
    changes made to a14789013d79b9bd91ab4ab21479207bc3bbe098.

--- Assets/Scripts/Maria/PlayerPush.cs
+++ Assets/Scripts/Maria/PlayerPush.cs
@@ -3,18 +3,9 @@ using UnityEngine;
 public class PlayerPush : MonoBehaviour
 {
     public float pushPower = 10f;
-    public StatesManager statesManager;
 
     private void OnControllerColliderHit(ControllerColliderHit hit)
     {
-        // Solo el estado grande puede abrir la puerta
-        if (statesManager.currentState != StatesManager.State.Big)
-            return;
-
-        // Solo afecta a objetos con la tag Door
-        if (!hit.collider.CompareTag("Door"))
-            return;
-
         Rigidbody body = hit.collider.attachedRigidbody;
 
         if (body == null || body.isKinematic)
